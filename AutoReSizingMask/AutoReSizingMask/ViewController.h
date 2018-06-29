6ecaadd2a9af2439fdce4ab267f1a2ee88d7943e
zhengjie19901102 <1058686106@qq.com>
2018-06-29 20:40:25 +0800

    添加AutoReSizingMask项目

---
+++ AutoReSizingMask/AutoReSizingMask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoReSizingMask
+//
+//  Created by 郑杰 on 2018/6/29.
+//  Copyright © 2018年 heiketu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoReSizingMask/AutoReSizingMask/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoReSizingMask
+//
+//  Created by 郑杰 on 2018/6/29.
+//  Copyright © 2018年 heiketu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
